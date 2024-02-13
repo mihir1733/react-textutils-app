d2f5bc197bc3966adf9901865cc3b1eea24dca4f
mihir1733 <mihirpithva17@gmail.com>
2024-02-13 13:08:53 +0530

    text summary bugs solved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function TextForm(props) {
-  const [text, setText] = useState('');
-  const [btnName, setBtnName] = useState('Copy Text');
+  const [text, setText] = useState("");
+  const [btnName, setBtnName] = useState("Copy Text");
   const convertToUppercase = () => {
     setText(text.toUpperCase());
   };
@@ -11,29 +11,40 @@ export default function TextForm(props) {
     setText(text.toLowerCase());
   };
   const removeExtraSpaces = () => {
-    setText(text.split(' ').filter(word => word).join(' ').trim());
+    setText(
+      text
+        .split(" ")
+        .filter((word) => word)
+        .join(" ")
+        .trim()
+    );
   };
   const clearText = () => {
-    setText('');
+    setText("");
   };
   const copyText = () => {
-    navigator.clipboard.writeText(text)
-    setBtnName('Copied')
+    navigator.clipboard.writeText(text);
+    setBtnName("Copied");
     setTimeout(() => {
-      setBtnName('Copy Text')
+      setBtnName("Copy Text");
     }, 2000);
   };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
   return (
-    <div className={`container text-${props.mode === 'dark' ? 'white' : 'black'}`}>
+    <div
+      className={`container text-${props.mode === "dark" ? "white" : "black"}`}
+    >
       <div className="container my-3 ">
         <div className="mb-3">
           <h1>{props.heading}</h1>
           <textarea
             className="form-control border-dark rounded"
-            style={{backgroundColor:props.mode === 'dark' ? 'black' : 'white',color:props.mode === 'dark' ? 'white' : 'black'}}
+            style={{
+              backgroundColor: props.mode === "dark" ? "black" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
             id="myText"
             rows="10"
             value={text}
@@ -41,30 +52,35 @@ export default function TextForm(props) {
           ></textarea>
         </div>
         <button
+          disabled={text.length === 0}
           className="btn btn-dark m-1"
           onClick={convertToUppercase}
         >
           Convert to Uppercase
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-dark m-1"
           onClick={convertToLowercase}
         >
           Convert to Lowercase
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-dark m-1"
           onClick={removeExtraSpaces}
         >
           Remove Extra Spaces
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-dark m-1"
           onClick={copyText}
         >
           {btnName}
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-dark m-1"
           onClick={clearText}
         >
@@ -73,7 +89,21 @@ export default function TextForm(props) {
       </div>
       <div className="container">
         <h4>Your text summary 🔎...</h4>
-        <p>{text.length === 0 ? 0 : text.split(' ').filter(word => word).length} words and {text.split(' ').filter(word => word).join(' ').trim().length} characters</p>
+        <p>
+          <small><i>Note: A space between each word is counted in characters</i></small><br />
+          {text.length === 0
+            ? 0
+            : text.split(/\s+/).filter((word) => word).length}{" "}
+          words and{" "}
+          {
+            text
+              .split(" ")
+              .filter((word) => word)
+              .join(" ")
+              .trim().length
+          }{" "}
+          characters
+        </p>
       </div>
     </div>
   );
